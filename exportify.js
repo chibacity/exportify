354998da8e7e6c16edcd351b1c37d7a8c5802f7a
Robert Steele <chibacity@users.noreply.github.com>
2017-01-15 11:34:10 -0500

    change spotify client id

--- exportify.js
+++ exportify.js
@@ -4,7 +4,7 @@ window.Helpers = {
 
     // Use Exportify application client_id if none given
     if (client_id == '') {
-      client_id = "9950ac751e34487dbbe027c4fd7f8e99"
+      client_id = "aec713c78ccb45b08c8e99a7a07cf2e0"
     }
 
     window.location = "https://accounts.spotify.com/authorize" +
